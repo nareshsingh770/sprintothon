b0b0b10500701dba65f90f43f4c97ac349efbf51
Naresh Singh <nsingh@gmail.com>
2025-12-21 21:15:06 +0530

    workflow file changes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
+  basePath: '/sprintothon',
+  assetPrefix: '/sprintothon/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
